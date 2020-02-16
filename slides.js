dde20b0b36087813b47f99e13c533f7a655e9a86
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-02-16 18:08:54 +0000

    Make slideshow automatic

--- slides.js
+++ slides.js
@@ -1,17 +1,28 @@
-let slideIndex = 1;
+let slideIndex = 0;
 showSlides(slideIndex);
+let timeoutHandle;
 
 // Next/previous controls
 function plusSlides(n) {
+    window.clearTimeout(timeoutHandle);
     showSlides(slideIndex += n);
+    timeoutHandle = window.setInterval(() => {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 4000);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+    window.clearTimeout(timeoutHandle);
     showSlides(slideIndex = n);
+    timeoutHandle = window.setInterval(() => {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 4000);
 }
 
-function showSlides(n) {
+async function showSlides(n) {
     let i;
     const slides = document.getElementsByClassName("mySlides");
     const dots = document.getElementsByClassName("dot");
@@ -23,7 +34,11 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    console.log(slides, slideIndex);
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+timeoutHandle = window.setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 4000);
